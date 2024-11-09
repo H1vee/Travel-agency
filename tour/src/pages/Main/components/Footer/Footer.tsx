0519fb258298a8744973bed90e881bfd4a6e70e1
ArtemLedinskyi <ledynskyi.artem@chnu.edu.ua>
2024-11-09 11:13:17 +0200

    Update footer

--- tour/src/pages/Main/components/Footer/Footer.tsx
+++ tour/src/pages/Main/components/Footer/Footer.tsx
@@ -1,38 +1,27 @@
-import {Divider} from "@nextui-org/react";
-import {Link} from "@nextui-org/react";
 import './Footer.scss'
 export const Footer =() =>{
     return(
-      <div>
-        <Divider/>
-        <div className="Footer">
-        <div className="Footer-Description">
-          <h4 className="">OpenWorld</h4>
-          <p className="Footer-Overview">Beautiful, fast and modern React UI library.</p>
-        </div>
-        
-        <div className="Footer-Option">
-          
-          <div className="">
-          <Link href="#" color="foreground" isBlock underline="hover" className="Footer-Main">
-          Головна
-          </Link>
+      <footer className='Footer'>
+          <div className='Footer-Container'>
+              <div className='Footer-row'>
+                  <div className='Footer-col'>
+                        <h4>Company</h4>
+                        <ul className='Footer-ListProp'>
+                          <li><a href="#">About Us</a></li>
+                          <li><a href="#">Our Services</a></li>
+                          <li><a href="#">Privacy Policy</a></li>
+                        </ul>
+                  </div>
+                  <div className='Footer-col'>
+                      <h4>Get Help</h4>
+                      <ul className='Footer-ListProp'>
+                          <li><a href="#">FAQ</a></li>
+                          <li><a href="#">Order Status</a></li>
+                          <li><a href="#">Payment Option</a></li>
+                        </ul>
+                  </div>
+              </div>
           </div>
-
-          <div className="">
-            <Link href="#" color="foreground" isBlock underline="hover" className="Footer-AboutUs">
-            Про Нас
-            </Link>
-          </div>
-
-          <div className="">
-            <Link href="#" color="foreground" isBlock underline="hover"className="Footer-Tours">
-            Тури
-            </Link>
-          </div>
-        </div>
-      </div>
-      </div>
-
+      </footer>
     );
-} 
+};
