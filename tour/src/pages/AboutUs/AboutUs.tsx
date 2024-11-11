c8804809cd2efe245243bc29d7829f2595ed6093
H1vee <ailedinskyi@gmail.com>
2024-11-11 14:03:17 +0200

    Add new page

---
+++ tour/src/pages/AboutUs/AboutUs.tsx
@@ -0,0 +1,10 @@
+import {Navbar} from '../../components/Navbar/Navbar';
+
+export const AboutUs =()=>{
+    return (
+        <div className="AboutUs">
+            <Navbar />
+        </div>
+    );
+
+}
