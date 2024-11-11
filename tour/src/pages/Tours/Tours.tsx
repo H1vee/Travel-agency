aee2e1480010e140a557002c1bc1e7d23c125da1
H1vee <ailedinskyi@gmail.com>
2024-11-11 22:41:35 +0200

    Add Navbar to page(Tours)

--- tour/src/pages/Tours/Tours.tsx
+++ tour/src/pages/Tours/Tours.tsx
@@ -1,9 +1,9 @@
-
+import {Navbar} from '../../components/Navbar/Navbar';
 export const Tours =()=>{
 
     return (
         <div className="Tours">
-
+            <Navbar></Navbar>
         </div>
     )
 };
