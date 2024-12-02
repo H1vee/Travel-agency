fdd8b7248e03e98023ed51e7839ac87c82d7b5ad
H1vee <ailedinskyi@gmail.com>
2024-12-02 14:11:49 +0200

    Add card image

--- tour/src/pages/Tours/components/Cards/Cards.tsx
+++ tour/src/pages/Tours/components/Cards/Cards.tsx
@@ -1,4 +1,4 @@
-import {Card, CardHeader, CardBody, Image} from "@nextui-org/react";
+import {Card, CardFooter, CardBody, Image} from "@nextui-org/react";
 import { useQuery } from "@tanstack/react-query";
 
 interface Tour {
@@ -25,5 +25,29 @@ export const Cards : React.FC =()=>{
   if(error){
     return <p>Error</p>
   }
-  
-}
+  if (isPending) {
+    return <p>Loading...</p>;
+  }
+  return(
+    <div className="gap-2 grid grid-cols-2 sm:grid-cols-4">
+      {data.map(tour=> (
+          <Card shadow="sm" isPressable onPress={() => console.log("item pressed")}>
+            <CardBody className="overflow-visible p-0">
+            <Image
+              shadow="sm"
+              radius="lg"
+              width="100%"
+              alt={tour.title}
+              className="w-full object-cover h-[140px]"
+              src={`http://127.0.0.1:1323${tour.imageSrc}`}
+            />
+            </CardBody>
+            <CardFooter className="text-small justify-between">
+            <b>{tour.title}</b>
+            <p className="text-default-500">20</p>
+          </CardFooter>
+          </Card>
+      ))}
+    </div>
+  );
+};
