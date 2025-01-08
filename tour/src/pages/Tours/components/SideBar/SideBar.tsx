ec0a0370e7e0a3ed94901af93fd2244c95fcf588
H1vee <ailedinskyi@gmail.com>
2025-01-08 19:08:45 +0200

    Add search bar and side bar

---
+++ tour/src/pages/Tours/components/SideBar/SideBar.tsx
@@ -0,0 +1,67 @@
+import {CheckboxGroup, Checkbox, Slider, SliderValue, Input, divider} from "@nextui-org/react";
+import "./SideBar.scss";
+import React from "react";
+
+export const SideBar =()=>{
+    const minValue = 0
+    const maxValue = 20000
+    const [sliderValue, setSliderValue] = React.useState([minValue, maxValue]);
+    const handleSliderChange = (newValue:any) => {
+        setSliderValue(newValue);
+      };
+    
+
+      const handleMinInputChange = (e:any) => {
+        const newMin = Math.min(Number(e.target.value), sliderValue[1]);
+        setSliderValue([newMin, sliderValue[1]]);
+      };
+    
+      const handleMaxInputChange = (e:any) => {
+        const newMax = Math.max(Number(e.target.value), sliderValue[0]);
+        setSliderValue([sliderValue[0], newMax]);
+      };
+    return(
+        
+        <div className="SideBar">
+        <CheckboxGroup label="Оберіть тур">
+        <Checkbox value="buenos-aires">Buenos Aires</Checkbox>
+        <Checkbox value="sydney">Sydney</Checkbox>
+        <Checkbox value="san-francisco">San Francisco</Checkbox>
+        <Checkbox value="london">London</Checkbox>
+        <Checkbox value="tokyo">Tokyo</Checkbox>
+      </CheckboxGroup>
+      <div className="SideBar-slider">
+        <div className="SliderBar-inputs">
+            <Input
+                endContent={
+                    <div className="pointer-events-none flex items-center">
+                        <span className="text-default-400 text-small">₴</span>
+                    </div>
+                    }
+            value={sliderValue[0].toString()}
+            onChange={handleMinInputChange}
+            />
+            <Input
+                endContent={
+                    <div className="pointer-events-none flex items-center">
+                        <span className="text-default-400 text-small">₴</span>
+                    </div>
+                    }
+              value={sliderValue[1].toString()}
+              onChange={handleMaxInputChange}
+            />
+        </div>
+        <Slider
+          label="Ціна"
+          maxValue={maxValue}
+          minValue={minValue}
+          step={0.01}
+          size="sm"
+          formatOptions={{style: "currency", currency: "UAH"}}
+          value={sliderValue}
+          onChange={handleSliderChange}
+        />
+      </div>
+    </div>
+    );
+}
