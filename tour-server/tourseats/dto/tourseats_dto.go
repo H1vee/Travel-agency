5c48e90894bab6daf3de286ce5834371c72d2312
H1vee <ailedinskyi@gmail.com>
2025-02-14 12:09:21 +0200

    added an API to get available seats

--- tour-server/tourseats/dto/tourseats_dto.go
+++ tour-server/tourseats/dto/tourseats_dto.go
@@ -2,6 +2,6 @@ package dto
 
 type TourSeatsDTO struct {
 	ID             uint `json:"id"`
-	TourDateID     uint `json:"tourDateID"`
-	AvailableSeats uint `json:"availableSeats"`
+	TourDateID     uint `json:"tour_date_id"`
+	AvailableSeats uint `json:"available_seats"`
 }
