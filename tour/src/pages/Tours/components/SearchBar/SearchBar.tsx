ffa52e8a44de3dd6f00f628ce7b8802d0cf405a3
H1vee <ailedinskyi@gmail.com>
2025-03-01 14:19:39 +0200

    Modified implementation of SearchBard.

--- tour/src/pages/Tours/components/SearchBar/SearchBar.tsx
+++ tour/src/pages/Tours/components/SearchBar/SearchBar.tsx
@@ -2,29 +2,21 @@ import React, { useState } from "react";
 import { Input, Button, Spacer, Card, Select, SelectItem } from "@heroui/react";
 import "./SearchBar.scss";
 
-export const countries = [
-    {key: "taiwan", label: "Тайвань"},
-    {key: "egypt", label: "Єгипет"},
-    {key:"boston",label:"Бостон"},
-    {key:"maldives",label:"Мальдіви"}
-  ];
-  
-  
+interface SearchBarProps {
+  setSearchQuery: (query: string) => void;
+}
 
-export const SearchBar: React.FC = () => {
-  const [searchQuery, setSearchQuery] = useState<string>("");
-
+export const SearchBar: React.FC<SearchBarProps> = ({ setSearchQuery }) => {
   return (
-      <div className="search-bar-body">
-        <Input
-          isClearable
-          fullWidth
-          placeholder="Введіть запит"
-          value={searchQuery}
-          onChange={(e) => setSearchQuery(e.target.value)}
-          size="lg"
-          className="search-input"
-        />
-      </div>
+    <div className="search-bar-body">
+      <Input
+        isClearable
+        fullWidth
+        placeholder="Введіть запит"
+        onChange={(e) => setSearchQuery(e.target.value)}
+        size="lg"
+        className="search-input"
+      />
+    </div>
   );
 };
