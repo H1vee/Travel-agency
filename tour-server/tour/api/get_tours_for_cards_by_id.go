2257ca056134b33220b1b66ddc74303b66c2e772
H1vee <ailedinskyi@gmail.com>
2025-03-01 16:42:52 +0200

    Fix query

--- tour-server/tour/api/get_tours_for_cards_by_id.go
+++ tour-server/tour/api/get_tours_for_cards_by_id.go
@@ -3,6 +3,8 @@ package api
 import (
 	"log"
 	"net/http"
+	"strconv"
+	"strings"
 	"tour-server/tour/dto"
 
 	"github.com/labstack/echo/v4"
@@ -11,15 +13,25 @@ import (
 
 func GetToursForCardsByID(db *gorm.DB) echo.HandlerFunc {
 	return func(c echo.Context) error {
-		ids := c.QueryParam("ids")
+		idsParam := c.QueryParam("ids")
 		if db == nil {
 			return c.JSON(http.StatusInternalServerError, map[string]string{"error": "Database connection is nil"})
+		}
+
+		idsStr := strings.Split(idsParam, ",")
+		var ids []int
+		for _, idStr := range idsStr {
+			id, err := strconv.Atoi(idStr)
+			if err != nil {
+				return c.JSON(http.StatusBadRequest, map[string]string{"error": "Invalid id format"})
+			}
+			ids = append(ids, id)
 		}
 		var toursWithImages []dto.TourCard
 		err := db.Debug().Table("tours").
 			Select("tours.id, tours.title, tours.price, COALESCE(tour_card_images.image_src, 'no-image.jpg') AS image_src").
 			Joins("LEFT JOIN tour_card_images ON tours.id = tour_card_images.tour_id").
-			Where("tours.id IN (" + ids + ")").
+			Where("tours.id IN ?", ids).
 			Find(&toursWithImages).Error
 
 		log.Printf("SQL Query executed: %+v\n", db.Statement.SQL.String())
