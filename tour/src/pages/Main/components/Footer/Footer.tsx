cd6292e4493cd7b83742120f4276015a44422fbc
H1vee <ailedinskyi@gmail.com>
2025-03-05 22:13:00 +0200

    Implemented Footer

--- tour/src/pages/Main/components/Footer/Footer.tsx
+++ tour/src/pages/Main/components/Footer/Footer.tsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import './Footer.scss';
+import { Logo } from '../../../../components/Logo/Logo';
 
 export const Footer: React.FC = () => {
   return (
     <footer className="footer">
       <div className="container">
-        
         <div className="footer-logo">
-          <img src="/path-to-your-logo.png" alt="Company Logo" />
+          <Logo></Logo>
         </div>
-
-        
+        <div className="footer-copyright">
+          © 2025 OpenWorld, Inc.
+        </div>
         <div className="footer-links">
           <a href="#!" className="footer-link">Terms</a>
           <a href="#!" className="footer-link">Privacy</a>
@@ -18,11 +19,6 @@ export const Footer: React.FC = () => {
           <a href="#!" className="footer-link">Status</a>
           <a href="#!" className="footer-link">Docs</a>
           <a href="#!" className="footer-link">Contact</a>
-        </div>
-
-        {/* Copyright */}
-        <div className="footer-copyright">
-          © 2025 OpenWorld, Inc.
         </div>
       </div>
     </footer>
