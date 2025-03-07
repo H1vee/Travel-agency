cba60f9f890174e3b3cb6deaaddd626f499358d2
H1vee <ailedinskyi@gmail.com>
2025-03-07 22:27:45 +0200

    Changed sql request to carousel

--- tour-server/tour/api/get_tours_carousel_by_id.go
+++ tour-server/tour/api/get_tours_carousel_by_id.go
@@ -11,16 +11,16 @@ import (
 
 func GetToursCarouselByID(db *gorm.DB) echo.HandlerFunc {
 	return func(c echo.Context) error {
-		var tour dto.TourCarousel
+		var tour []dto.TourCarousel
 		id := c.Param("id")
 		if db == nil {
 			return c.JSON(http.StatusInternalServerError, map[string]string{
 				"error": "Database connection is nil"})
 		}
 		err := db.Table("tour_gallery_images").Debug().
-			Select(`*`).
-			Where("tours.id = ?", id).
-			Scan(&tour).Error
+			Select(`tour_gallery_images.*`).
+			Where("tour_gallery_images.tour_id = ?", id).
+			Find(&tour).Error
 
 		if err != nil {
 			log.Printf("Failed to fetch tour carousel: %v\n", err)
