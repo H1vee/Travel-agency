fa7edc617a3eeaf0884a92520ef486b3398d7547
H1vee <ailedinskyi@gmail.com>
2025-03-08 09:56:00 +0200

    Set up TourSwiper

--- tour/src/pages/TourDetails/components/Swiper/TourSwiper.tsx
+++ tour/src/pages/TourDetails/components/Swiper/TourSwiper.tsx
@@ -1,36 +1,29 @@
 import { Swiper as SwiperBase, SwiperSlide } from 'swiper/react';
 import { Pagination, A11y, Navigation} from 'swiper/modules';
-import { Button } from "@heroui/react";
 import { useQuery } from '@tanstack/react-query';
 import 'swiper/css';
 import 'swiper/css/pagination';
 import 'swiper/css/navigation'
+import { useParams } from 'react-router';
 
 interface Tour {
-    id           :number   
-    title        :string  
-    description  :string  
-    callToAction :string  
-    imageSrc     :string  
+    tourID : number
+    image_src: string
   }
 export const TourSwiper: React.FC =()=>{
+  const {id} = useParams();
     const { isPending, error, data } = useQuery({
-        queryKey: ['toursData'],
+        queryKey: ['toursData',id],
         queryFn: async() => {
-          const fetched = await fetch('/api/tourswiper');
+          const fetched = await fetch(`/api/tour-carousel/${id}`);
+          if (!fetched.ok) throw new Error("Tour not found");
           const tours: Tour[] = await fetched.json();
           return tours;
         },
       })
-    
-      if (isPending) {
-        return <p>Pending..</p>
-      }
-    
-      if (error) {
-        return <p>Error</p>;
-      }
-    
+      if (isPending) return <p>Loading...</p>;
+      if (error) return <p>Error: {error.message}</p>;
+      if (!data) return <p>Tour not found</p>;
     return(
         <SwiperBase
         modules={[Pagination, A11y,Navigation]}
@@ -40,8 +33,8 @@ export const TourSwiper: React.FC =()=>{
         pagination={{ clickable: true }}
         className={'Swiper'}
       >
-        {data.slice(0,4).map(tour => (
-          <SwiperSlide key={tour.id} className={'Swiper-Slide'} style={{backgroundImage: `url("http://localhost:1323${tour.imageSrc}")`}}>
+        {data.slice(0,4).map((tour, index) => (
+          <SwiperSlide key={`${tour.tourID}-${index}`} className={'Swiper-Slide'} style={{ backgroundImage: `url("http://localhost:1323${tour.image_src}")`}}>
         
           </SwiperSlide>
         ))}
